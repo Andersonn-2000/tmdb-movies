ac67d16f293daf5192a6afad714bc7dc73ae4709
Anderson Mendes <andersonmendess575@gmail.com>
2026-04-22 15:49:12 -0300

    refactor: save() function update

--- cortex/src/exporter.py
+++ cortex/src/exporter.py
@@ -1,14 +1,24 @@
-import os
 import pandas as pd
 from pathlib import Path
 
 class CSVExporter:
     def __init__(self, filename: Path | str):
-        self.filename = filename
+        self.filename = Path(filename)
+        self.header = False
     
     def save(self, data) -> None:
 
-        os.makedirs(os.path.dirname(self.filename), exist_ok=True)
+        if not data:
+            return
+        
+        self.filename.parent.mkdir(parents=True, exist_ok=True)
 
         df = pd.DataFrame(data=data)
-        df.to_csv(self.filename, index=False, encoding="utf-8")
+        df.to_csv(
+            self.filename,
+            mode='a',
+            header= not self.header,
+            index=False,
+            encoding="utf-8")
+        
+        self.header = True
